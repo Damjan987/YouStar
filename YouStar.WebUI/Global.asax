a77336911c35ba878912f95c6d3d14e1c1379b59
Damjan <jasamprogramer7@gmail.com>
2019-07-25 14:27:37 +0100

    Initial Solution Setup

---
+++ YouStar.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YouStar.WebUI.MvcApplication" Language="C#" %>
---
+++ YouStar.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YouStar.WebUI.Startup))]
+namespace YouStar.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
