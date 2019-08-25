b7db8be01a16be49583c71abbbe969561e57d791
Damjan <jasamprogramer7@gmail.com>
2019-08-25 22:42:58 +0100

    Added Entity Framework

---
+++ YouStar.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouStar.Core.Models;
+
+namespace YouStar.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() 
+            : base("DefaultConnection") {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ YouStar.DataAccess.SQL/Migrations/201908252138569_Initial.cs
@@ -0,0 +1,46 @@
+namespace YouStar.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Description = c.String(maxLength: 3000),
+                        Image = c.String(),
+                        Stars = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                        ConfirmPassword = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Posts");
+        }
+    }
+}
